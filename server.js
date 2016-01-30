a4e60bcda6a635e8bd7d584ce9b070417bef18f5
Chris St. John <christophersstjohn@gmail.com>
2016-01-30 18:38:25 +0800

    Added item

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 var express = require ('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+  id:'1',
+  descripton:'Meet mom for lunch',
+  completed: false
+}, {
+  id:'2',
+  descripton:'Go to market',
+  completed: false
+}, {
+  id:'3',
+  descripton:'Go to movie',
+  completed: true
+}];
 
 app.get ('/',function (req,res) {
   res.send ('TODO API Root');
 });
+
+// GET /todos
+app.get('/todos',function (req,res) {
+  res.json(todos);
+});
+
+// GET /todos/id
 
 app.listen(PORT, function () {
   console.log('Express listening on port: ' + PORT + '!');
